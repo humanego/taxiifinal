17e401b7d2eb629c354bf6c03887da095af68de2
ilham <ilham@ILHAMPC>
2023-03-20 23:19:35 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxiifinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            taxigas taxi = new taxigas();
+
+            taxi.DriveName = "Ilham";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 84785478;
+
+            taxi.info();
+            taxi.up();
+            taxi.down();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ taxigas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxiifinal
+{
+    internal class taxigas
+    {
+
+        public string DriveName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void info()
+        {
+            Console.WriteLine("\n Taxi Info \n");
+            Console.WriteLine("Nama Driver : {0}", DriveName);
+
+            if (OnDuty == true)
+            {
+                Console.WriteLine("Kerja : Iya ", OnDuty);
+
+            }
+            else
+            {
+                Console.WriteLine("Kerja : Tidak", OnDuty);
+            }
+            Console.WriteLine("Nomor Pelanggan : {0}", NumPassenger);
+
+        }
+        public void up()
+        {
+            Console.WriteLine("\n {0} Sedang Menaikkan Penumpang", DriveName);
+        }
+        public void down()
+        {
+            Console.WriteLine("\n {0} Sedang Menurunkan Penumpang", DriveName);
+        }
+    }
+}
